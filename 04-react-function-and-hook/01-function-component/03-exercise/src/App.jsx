4a9426fbac8da492db5bba49bde22ebd30f9c1d5
NutyadaS <nutyada.s7810@gmail.com>
2023-04-10 11:41:43 +0200

    practice exercise 3

--- 04-react-function-and-hook/01-function-component/03-exercise/src/App.jsx
+++ 04-react-function-and-hook/01-function-component/03-exercise/src/App.jsx
@@ -1,3 +1,38 @@
 import React from "react";
 
-<img src = 
+const App = () => {
+  return (
+    <div id="app">
+      <img
+        width="150px"
+        height="150px"
+        src="https://i.pinimg.com/originals/ae/24/87/ae24874dd301843548c034a3d2973658.png"
+      />
+      <Info />
+      <Social />
+    </div>
+  );
+};
+
+const Info = () => {
+  let name = "Mai";
+  let bio = "This is a bio";
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{bio}</p>
+    </div>
+  );
+};
+
+const Social = () => {
+  return (
+    <div>
+      <a href="">Facebook</a>
+      <br></br>
+      <a href="">Twitter</a>
+    </div>
+  );
+};
+
+export default App;
