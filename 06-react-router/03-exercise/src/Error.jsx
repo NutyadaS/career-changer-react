0f7dd9d8bf70345bf6dfac7f4560f4d019c5cd4f
NutyadaS <nutyada.s7810@gmail.com>
2023-04-12 06:54:48 +0200

    Update practice 06 react router 03 exercise add signup, login & error

---
+++ 06-react-router/03-exercise/src/Error.jsx
@@ -0,0 +1,11 @@
+import Layout from "./Layout";
+
+const Error = () => {
+  return (
+    <Layout>
+      <div>Error</div>
+    </Layout>
+  );
+};
+
+export default Error;
--- 06-react-router/03-exercise/src/main.jsx
+++ 06-react-router/03-exercise/src/main.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './assets/style/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./assets/style/index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import Error from "./Error";
 
 const router = createBrowserRouter([
   // insert your path here
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/error",
+    element: <Error />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
